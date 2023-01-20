6956918ea61b7fb1db97bf4d096ed4f2177d1653
George V <georgev22.github@gmail.com>
2023-01-20 17:26:31 +0200

    feat(LoggerWrapper): Added SLF4J Logger

--- src/main/java/com/georgev22/library/utilities/LoggerWrapper.java
+++ src/main/java/com/georgev22/library/utilities/LoggerWrapper.java
@@ -8,11 +8,19 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class LoggerWrapper extends java.util.logging.Logger {
-    private final Logger base;
+    private final Logger log4j;
+    private final org.slf4j.Logger slf4j;
 
     public LoggerWrapper(Logger logger) {
         super("logger", null);
-        this.base = logger;
+        this.log4j = logger;
+        this.slf4j = null;
+    }
+
+    public LoggerWrapper(org.slf4j.Logger logger) {
+        super("logger", null);
+        this.log4j = null;
+        this.slf4j = logger;
     }
 
     @Override
@@ -22,30 +30,72 @@ public class LoggerWrapper extends java.util.logging.Logger {
 
     @Override
     public void log(Level level, String msg) {
-        if (level == Level.FINE)
-            base.debug(msg);
-        else if (level == Level.WARNING)
-            base.warn(msg);
-        else if (level == Level.SEVERE)
-            base.error(msg);
-        else if (level == Level.INFO)
-            base.info(msg);
-        else
-            base.trace(msg);
+        if (level == Level.FINE) {
+            if (log4j != null) {
+                log4j.debug(msg);
+            } else if (slf4j != null) {
+                slf4j.debug(msg);
+            }
+        } else if (level == Level.WARNING) {
+            if (log4j != null) {
+                log4j.warn(msg);
+            } else if (slf4j != null) {
+                slf4j.warn(msg);
+            }
+        } else if (level == Level.SEVERE) {
+            if (log4j != null) {
+                log4j.error(msg);
+            } else if (slf4j != null) {
+                slf4j.error(msg);
+            }
+        } else if (level == Level.INFO) {
+            if (log4j != null) {
+                log4j.info(msg);
+            } else if (slf4j != null) {
+                slf4j.info(msg);
+            }
+        } else {
+            if (log4j != null) {
+                log4j.trace(msg);
+            } else if (slf4j != null) {
+                slf4j.trace(msg);
+            }
+        }
     }
 
     @Override
     public void log(Level level, String msg, Object param1) {
-        if (level == Level.FINE)
-            base.debug(msg, param1);
-        else if (level == Level.WARNING)
-            base.warn(msg, param1);
-        else if (level == Level.SEVERE)
-            base.error(msg, param1);
-        else if (level == Level.INFO)
-            base.info(msg, param1);
-        else
-            base.trace(msg, param1);
+        if (level == Level.FINE) {
+            if (log4j != null) {
+                log4j.debug(msg, param1);
+            } else if (slf4j != null) {
+                slf4j.debug(msg, param1);
+            }
+        } else if (level == Level.WARNING) {
+            if (log4j != null) {
+                log4j.warn(msg, param1);
+            } else if (slf4j != null) {
+                slf4j.warn(msg, param1);
+            }
+        } else if (level == Level.SEVERE) {
+            if (log4j != null) {
+                log4j.error(msg, param1);
+            } else if (slf4j != null) {
+                slf4j.error(msg, param1);
+            }
+        } else if (level == Level.INFO) {
+            if (log4j != null) {
+                log4j.info(msg, param1);
+            } else if (slf4j != null) {
+                slf4j.info(msg, param1);
+            }
+        } else {
+            if (log4j != null) {
+                log4j.trace(msg, param1);
+            } else if (slf4j != null) {
+                slf4j.trace(msg, param1);
+            }
+        }
     }
 
     @Override
@@ -55,16 +105,37 @@ public class LoggerWrapper extends java.util.logging.Logger {
 
     @Override
     public void log(Level level, String msg, Throwable params) {
-        if (level == Level.FINE)
-            base.debug(msg, params);
-        else if (level == Level.WARNING)
-            base.warn(msg, params);
-        else if (level == Level.SEVERE)
-            base.error(msg, params);
-        else if (level == Level.INFO)
-            base.info(msg, params);
-        else
-            base.trace(msg, params);
+        if (level == Level.FINE) {
+            if (log4j != null) {
+                log4j.debug(msg, params);
+            } else if (slf4j != null) {
+                slf4j.debug(msg, params);
+            }
+        } else if (level == Level.WARNING) {
+            if (log4j != null) {
+                log4j.warn(msg, params);
+            } else if (slf4j != null) {
+                slf4j.warn(msg, params);
+            }
+        } else if (level == Level.SEVERE) {
+            if (log4j != null) {
+                log4j.error(msg, params);
+            } else if (slf4j != null) {
+                slf4j.error(msg, params);
+            }
+        } else if (level == Level.INFO) {
+            if (log4j != null) {
+                log4j.info(msg, params);
+            } else if (slf4j != null) {
+                slf4j.info(msg, params);
+            }
+        } else {
+            if (log4j != null) {
+                log4j.trace(msg, params);
+            } else if (slf4j != null) {
+                slf4j.trace(msg, params);
+            }
+        }
     }
 
 }

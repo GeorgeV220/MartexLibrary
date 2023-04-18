003363672e77015e6b68a33c57c68a57a61ca248
George V <georgev22.github@gmail.com>
2023-04-18 19:01:49 +0300

    chore(LoggerWrapper): add Javadocs documentation
    This commit adds comprehensive Javadoc documentation to the `LoggerWrapper` class. The Javadocs provide better code readability, maintainability and ease of understanding of the class. The `LoggerWrapper` class wraps log4j and SLF4J to Java logger, making it a crucial component of the library.

--- utilities/src/main/java/com/georgev22/library/utilities/LoggerWrapper.java
+++ utilities/src/main/java/com/georgev22/library/utilities/LoggerWrapper.java
@@ -1,33 +1,58 @@
 package com.georgev22.library.utilities;
 
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.MessageFormat;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
-public class LoggerWrapper extends java.util.logging.Logger {
-    private final Logger log4j;
+/**
+ * LoggerWrapper is a wrapper class used to integrate java.util.Logger and log4j/slf4j loggers.
+ * It takes logger as an input and logs the records based on their levels.
+ */
+public class LoggerWrapper extends Logger {
+    private final org.apache.logging.log4j.Logger log4j;
     private final org.slf4j.Logger slf4j;
 
-    public LoggerWrapper(Logger logger) {
+    /**
+     * Constructor to create a LoggerWrapper wrapping the given Log4j Logger object.
+     *
+     * @param logger A Log4j Logger object.
+     */
+    public LoggerWrapper(org.apache.logging.log4j.Logger logger) {
         super("logger", null);
         this.log4j = logger;
         this.slf4j = null;
     }
 
+    /**
+     * Constructor to create a LoggerWrapper wrapping the given SLF4J Logger object.
+     *
+     * @param logger An SLF4J Logger object.
+     */
     public LoggerWrapper(org.slf4j.Logger logger) {
         super("logger", null);
         this.log4j = null;
         this.slf4j = logger;
     }
 
+    /**
+     * Logs a message at the given LogRecord level.
+     *
+     * @param record A LogRecord object containing the log level and message.
+     */
     @Override
     public void log(@NotNull LogRecord record) {
         log(record.getLevel(), record.getMessage());
     }
 
+    /**
+     * Logs a message at the specified log level.
+     *
+     * @param level The log level.
+     * @param msg   The message to be logged.
+     */
     @Override
     public void log(Level level, String msg) {
         if (level == Level.FINE) {
@@ -63,6 +88,13 @@ public class LoggerWrapper extends java.util.logging.Logger {
         }
     }
 
+    /**
+     * Logs a message with a single parameter at the specified log level.
+     *
+     * @param level  The log level.
+     * @param msg    The message to be logged.
+     * @param param1 The single parameter.
+     */
     @Override
     public void log(Level level, String msg, Object param1) {
         if (level == Level.FINE) {
@@ -98,11 +130,25 @@ public class LoggerWrapper extends java.util.logging.Logger {
         }
     }
 
+    /**
+     * Logs a message with multiple parameters at the specified log level.
+     *
+     * @param level  The log level.
+     * @param msg    The message to be logged.
+     * @param params An array of parameters.
+     */
     @Override
     public void log(Level level, String msg, Object[] params) {
         log(level, MessageFormat.format(msg, params)); // workaround not formatting correctly
     }
 
+    /**
+     * Logs a message with a Throwable at the specified log level.
+     *
+     * @param level  The log level.
+     * @param msg    The message to be logged.
+     * @param params The Throwable associated with the message.
+     */
     @Override
     public void log(Level level, String msg, Throwable params) {
         if (level == Level.FINE) {

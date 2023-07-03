24b0c3166a30a86f4d8ac7bc749bd92e0af7f455
George V <georgev22.github@gmail.com>
2023-07-03 23:09:19 +0300

    refactor: Simplify ActionManager and Action interface

--- minecraft/bukkit/src/main/java/com/georgev22/library/minecraft/inventory/utils/actions/Action.java
+++ minecraft/bukkit/src/main/java/com/georgev22/library/minecraft/inventory/utils/actions/Action.java
@@ -1,17 +1,9 @@
 package com.georgev22.library.minecraft.inventory.utils.actions;
 
-import com.georgev22.library.exceptions.ActionRunException;
-import com.georgev22.library.maps.ObjectMap;
-import org.bukkit.OfflinePlayer;
-
-import java.util.List;
+import com.georgev22.library.maps.ConcurrentObjectMap;
 
 public interface Action {
+    void runAction();
 
-    void runAction(OfflinePlayer offlinePlayer) throws ActionRunException;
-
-    String name();
-
-    List<ObjectMap.Pair<String, List<Object>>> data();
-
+    ConcurrentObjectMap<String, Object> getData();
 }

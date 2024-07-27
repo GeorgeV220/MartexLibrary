4f9abdd18402b3186994355f1b160122356fc9cf
George V. <georgev22.github@gmail.com>
2024-07-27 22:10:45 +0300

    refactor: remove @Column annotation on name field

--- database/src/main/java/com/georgev22/library/utilities/GenericEntity.java
+++ database/src/main/java/com/georgev22/library/utilities/GenericEntity.java
@@ -1,11 +1,9 @@
 package com.georgev22.library.utilities;
 
-import com.georgev22.library.utilities.annotations.Column;
 import org.jetbrains.annotations.NotNull;
 
 public class GenericEntity extends Entity {
 
-    @Column(name = "name", type = "VARCHAR(255)")
     public String name;
 
     public GenericEntity(String _id) {
